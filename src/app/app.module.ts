c00a755bbb62dffa4bfe18a76b54393a23e81fc4
Petar <petar.krastev@haw-hamburg.de>
2018-01-24 00:10:56 +0200

    [ADD] Bootstrap and font-awesome dependency [ADD] Value component, used only for testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {ValueComponent} from './value/value.component';
+import {HttpModule} from '@angular/http';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ValueComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/value/value.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {Http} from '@angular/http';
+
+/**
+ * Dummy component used only for testing!
+ * TODO: Remove in the final version
+ */
+@Component({
+  selector: 'app-value',
+  templateUrl: './value.component.html',
+  styleUrls: ['./value.component.css']
+})
+export class ValueComponent implements OnInit {
+  values: any;
+
+  constructor(private http: Http) { }
+
+  ngOnInit() {
+    this.getValues();
+  }
+
+  getValues() {
+    this.http.get('http://localhost:5000/api/values').subscribe(response => {
+      this.values = response.json();
+    });
+  }
+
+}
